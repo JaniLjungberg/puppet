654414554f0c50e882d758e9c62a31c399777538
Jani Ljungberg <janiljungberg@outlook.com>
2017-04-10 02:03:19 +0300

    Commit apache toimii

--- modules/apache2/manifests/init.pp
+++ modules/apache2/manifests/init.pp
@@ -1,11 +1,12 @@
 class apache2 {
-	package {"apache2":
-		ensure =>"installed",
+
+	package {'apache2':
+		ensure => installed
 	}
 
-	service {"apache2":
-		ensure => "running",
-		enable => "true"
-		require => Package["apache2"]
+	service {'apache2':
+		ensure => running,
+		enable => true,
+		require => Package['apache2'],
 	}
 }
