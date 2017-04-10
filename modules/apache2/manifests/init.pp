91eb147dfa8af634e95cbeda268660ae27963b17
Jani Ljungberg <janiljungberg@outlook.com>
2017-04-10 03:09:03 +0300

    Kotiläksy 2 tehty

--- modules/apache2/manifests/init.pp
+++ modules/apache2/manifests/init.pp
@@ -4,9 +4,20 @@ class apache2 {
 		ensure => installed
 	}
 
+	file {'/etc/apache2/apache2.conf':
+		content => template("apache2/apache2.conf"),
+		require => Package["apache2"],
+		notify => Service["apache2"],
+	}
+
 	service {'apache2':
 		ensure => running,
 		enable => true,
 		require => Package['apache2'],
 	}
+
 }
+
+
+
+
