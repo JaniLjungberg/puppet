c5efdee0e510a835b92ac48b8784dd1c0fdd6359
Jani Ljungberg <janiljungberg@outlook.com>
2017-04-16 01:19:05 +0300

    Tehtävä 3 done

---
+++ modules/omasivu/manifests/init.pp
@@ -0,0 +1,39 @@
+class omasivu {
+
+	exec {"apt-get update":
+		command => "/usr/bin/apt-get update",
+	}
+
+	package {'apache2':
+		ensure => 'installed',
+	}
+
+	service {'apache2':
+		ensure => 'running',
+		enable => 'true',
+		require => Package["apache2"],
+	}
+	
+	file {'/home/jani/kotisivu/index.html':
+		content => template("omasivu/index.html"),
+	}
+
+	file {'/etc/apache2/sites-available/omasivu.conf':
+		content => template("omasivu/omasivu.conf"),
+		require => Package['apache2'],
+		notify => Service['apache2'],
+	}
+
+	file {'/etc/apache2/sites-enabled/omasivu.conf':
+		ensure => 'link',
+		target => '/etc/apache2/sites-available/omasivu.conf',
+		require => Package["apache2"],
+		notify => Service["apache2"],
+	}
+
+	file {'/etc/apache2/sites-enabled/000-default.conf':
+		ensure => 'absent',
+		notify => Service["apache2"],
+	}
+
+}
